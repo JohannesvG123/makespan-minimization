3cb136ad69c3c9b7a30b85c24807011be924af6b
Johannes von Geisau <johannes.von.geisau@t-online.de>
2024-01-29 13:44:22 +0100

    small fix

--- src/run-all.py
+++ src/run-all.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import sys
 import time
 
@@ -14,16 +15,20 @@ if len(sys.argv) < 2:
 args = sys.argv[1]
 
 os.system('cargo build')
+mm = ''
+if 'Windows' in platform.platform():
+    mm = 'target\debug\makespan-minimization.exe '
+else:
+    mm = 'target/debug/makespan-minimization '
 
 start_time = time.time()
 
 for file in os.listdir("./benchmarks"):
     if os.path.isfile('./benchmarks/' + file) and ".txt" in file:
         print("starting with input: '" + file + "'")
-        os.system(
-            'target\debug\makespan-minimization.exe --path ' + './benchmarks/' + file + ' ' + args + ' --write --write-separate-files')
-        # os.system('target\debug\makespan-minimization.exe --path ' + './benchmarks/' + file + ' --bf --lpt --rr --rf --swap --ff --rf-configs , --swap-configs ,, --write --write-separate-files')
-        # os.system('cargo run --bin makespan-minimization -- --path ' + file + ' --bf --lpt --rr --rf --swap --ff --rf-configs , --swap-configs ,, --write --write-separate-files')
+
+        os.system(mm + '--path ' + './benchmarks/' + file + ' ' + args + ' --write --write-separate-files')
+
         print("end with input: '" + file + "' -----------------------\n")
 
 end_time = time.time()
