0bb26dc2a9385d8026593d9411197cc3250c5ade
Johannes von Geisau <johannes.von.geisau@t-online.de>
2024-01-29 13:23:33 +0100

    small fix

--- src/run-all.py
+++ src/run-all.py
@@ -21,7 +21,7 @@ for file in os.listdir("./benchmarks"):
     if os.path.isfile('./benchmarks/' + file) and ".txt" in file:
         print("starting with input: '" + file + "'")
         os.system(
-            'target\debug\makespan-minimization.exe --path ' + './benchmarks/' + file + ' ' + args + ' --write --write-separate-files"')
+            'target\debug\makespan-minimization.exe --path ' + './benchmarks/' + file + ' ' + args + ' --write --write-separate-files')
         # os.system('target\debug\makespan-minimization.exe --path ' + './benchmarks/' + file + ' --bf --lpt --rr --rf --swap --ff --rf-configs , --swap-configs ,, --write --write-separate-files')
         # os.system('cargo run --bin makespan-minimization -- --path ' + file + ' --bf --lpt --rr --rf --swap --ff --rf-configs , --swap-configs ,, --write --write-separate-files')
         print("end with input: '" + file + "' -----------------------\n")
