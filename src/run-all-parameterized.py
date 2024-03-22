4c598470f98e0ec3f4d5cecc3e0605168597b572
Johannes von Geisau <johannes.von.geisau@t-online.de>
2024-03-22 17:12:21 +0100

    more tests

--- src/run-all-parameterized.py
+++ src/run-all-parameterized.py
@@ -15,7 +15,7 @@ def find_indices(search_list, search_item):
 
 
 def run_all():
-    os.system('cargo build')
+    # os.system('cargo build')
     mm = ''
     if 'Windows' in platform.platform():
         mm = 'target\debug\makespan-minimization.exe '
@@ -36,9 +36,12 @@ def run_all():
         indices = find_indices(files, files_wo_seed[i])
         i1 = indices.pop(random.randint(0, indices.__len__() - 1))
         files_subset.append(files[i1])
-        # if indices.__len__() > 0: #AUSKOMMENTIEREN FÜR GRÖSSERES TESTSET
-        #     i2 = indices.pop(random.randint(0, indices.__len__() - 1))
-        #     files_subset.append(files[i2])
+        if indices.__len__() > 0:  # AUSKOMMENTIEREN FÜR GRÖSSERES TESTSET
+            i2 = indices.pop(random.randint(0, indices.__len__() - 1))
+            files_subset.append(files[i2])
+        if indices.__len__() > 1:  # AUSKOMMENTIEREN FÜR GRÖSSERES TESTSET
+            i3 = indices.pop(random.randint(0, indices.__len__() - 1))
+            files_subset.append(files[i3])
 
     os.system(f"{files_subset} >> logs/instances.txt")
     inst = open(f"logs/instances.txt", 'a')
@@ -46,24 +49,29 @@ def run_all():
     inst.close()
 
     configs = [
-        "two-job-random-swap,all,max",
-        "two-job-random-swap,improvement,max",
-        "two-job-random-swap,decline-by-5%-chance,max",
-        "two-job-random-swap,decline-by-20%-chance,max",
-        "two-job-random-swap,decline-by-50%-chance,max",
         "two-job-brute-force,improvement,max",
-        "two-job-brute-force,decline-by-5%-chance,max",
-        "two-job-brute-force,decline-by-20%-chance,max",
-        "two-job-brute-force,decline-by-50%-chance,max"]
+        "two-job-random-swap,improvement,max",
+        "mix"]
 
-    for timeout_after in [60]:
-        for num_solutions in [500, 5000]:
+    for timeout_after in [300]:
+        for num_solutions in [5000]:
             for config in configs:
-                for num_threads in [1, 4, 8, 16]:
+                for num_threads in [16, 8, 4, 1]:
+                    if config == "mix" and num_threads == 4:
+                        break
+                    configg = ""
+                    if config == "mix":
+                        if num_threads == 8:
+                            configg = "two-job-random-swap,all,1 two-job-random-swap,decline-by-5%-chance,1 two-job-random-swap,decline-by-20%-chance,1 two-job-random-swap,decline-by-50%-chance,1 two-job-brute-force,improvement,1 two-job-brute-force,decline-by-5%-chance,1 two-job-brute-force,decline-by-20%-chance,1 two-job-brute-force,decline-by-50%-chance,1"
+                        if num_threads == 16:
+                            configg = "two-job-random-swap,all,1 two-job-random-swap,decline-by-5%-chance,1 two-job-random-swap,decline-by-20%-chance,1 two-job-random-swap,decline-by-50%-chance,1 two-job-brute-force,improvement,1 two-job-brute-force,decline-by-5%-chance,1 two-job-brute-force,decline-by-20%-chance,1 two-job-brute-force,decline-by-50%-chance,1 two-job-random-swap,all,1 two-job-random-swap,decline-by-5%-chance,1 two-job-random-swap,decline-by-20%-chance,1 two-job-random-swap,decline-by-50%-chance,1 two-job-brute-force,improvement,1 two-job-brute-force,decline-by-5%-chance,1 two-job-brute-force,decline-by-20%-chance,1 two-job-random-swap,improvement,1"
+                    else:
+                        configg = configg
                     i = 0
 
-                    args = f"--num-threads {num_threads} --num-solutions {num_solutions} --timeout-after {timeout_after} --swap-configs {config} --bf --ff --lpt --rr --swap --rf --rf-configs , , , ,"
-                    name = f"{num_threads}-threads,{num_solutions}-sol,{timeout_after}s-timeout,{config}"
+                    args = f"--num-threads {num_threads} --num-solutions {num_solutions} --timeout-after {timeout_after} --swap-configs {configg} --bf --ff --lpt --rr --swap --rf --rf-configs , , , ,"
+                    name = f"{num_threads}-threads,{num_solutions}-sol,{timeout_after}s-timeout,{configg}"
+                    print(name)
 
                     prog = open(f"logs/progress.txt", 'a')
                     prog.write(f"running: {name}")
