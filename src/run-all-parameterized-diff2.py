675951e40ccd48d98427c60c9fe9b1a4264d1b76
Johannes von Geisau <johannes.von.geisau@t-online.de>
2024-03-31 19:54:33 +0200

    testinggg

---
+++ src/run-all-parameterized-diff2.py
@@ -0,0 +1,93 @@
+import os
+import platform
+import random
+import subprocess
+import time
+
+
+# working directory must be "/makespan-minimization"!
+
+# run config on all instances (==.txt files) in benchmarks directory
+# example config: "--bf --lpt --rr --rf --swap --ff --rf-configs , --swap-configs ,,"
+
+def find_indices(search_list, search_item):
+    return [index for (index, item) in enumerate(search_list) if search_item in item]
+
+
+def run_all():
+    # os.system('cargo build')
+    mm = ''
+    if 'Windows' in platform.platform():
+        mm = 'target\debug\makespan-minimization.exe '
+    else:
+        mm = 'target/debug/makespan-minimization '
+
+    files = os.listdir("./benchmarks")
+    files_wo_seed = []
+    files_subset = []  # wird am Ende 2 instanzen jeder größer enthalten (mit 2 verschiedenen seeds):
+    for file in files:
+        if not (os.path.isfile('./benchmarks/' + file) and ".txt" in file):
+            files.remove(file)
+        else:
+            s = file.split("-seed")[0]
+            if not files_wo_seed.__contains__(s):
+                files_wo_seed.append(s)
+    for i in range(files_wo_seed.__len__()):
+        indices = find_indices(files, files_wo_seed[i])
+        i1 = indices.pop(random.randint(0, indices.__len__() - 1))
+        files_subset.append(files[i1])
+        # if indices.__len__() > 0: #AUSKOMMENTIEREN FÜR GRÖSSERES TESTSET
+        #     i2 = indices.pop(random.randint(0, indices.__len__() - 1))
+        #     files_subset.append(files[i2])
+
+    os.system(f"{files_subset} >> logstt/instances.txt")
+    inst = open(f"logstt/instances.txt", 'a')
+    inst.write(f"{files_subset.__len__()}{files_subset}\n")
+    inst.close()
+
+    configs = ["two-job-random-swap,improvement,max"]
+
+    for timeout_after in [150]:
+        for num_solutions in [5, 50, 500, 5000]:
+            for config in configs:
+                for num_threads in [1, 4, 8]:
+                    i = 0
+
+                    args = f"--num-threads {num_threads} --num-solutions {num_solutions} --timeout-after {timeout_after} --swap-configs {config} --bf --ff --lpt --rr --swap --rf --rf-configs , , , ,"
+                    name = f"diff,{num_threads}-threads,{num_solutions}-sol,{timeout_after}s-timeout,{config}"
+
+                    prog = open(f"logstt/progress.txt", 'a')
+                    prog.write(f"running: {name}")
+                    prog.close()
+
+                    start_time = time.time()
+
+                    s = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
+                    logs = open(f"logstt/logs_{s}.txt", 'a')
+                    logs.write(f"\nFRAMEWORK_CONFIG: {args}\n")
+                    logs.write(f"NAME: {name}\n")
+                    logs.write(f"{s}\n")
+                    logs.close()
+                    logs = open(f"logstt/logs_{s}.txt", 'a')
+
+                    for file in files_subset:
+                        print(f"{i}.: starting with input: '" + file + "'")
+
+                        # os.system(f"{mm}--path ./benchmarks/{file} {args} --measurement >> logstt/logs_{s}.txt 2>&1 &")  # --write --write-separate-files DAS BRAUCHT MAN FÜR WINDOWS!
+                        subprocess.run([f"./{mm}--path ./benchmarks/{file} {args} --measurement"], stdout=logs,
+                                       stderr=logs, shell=True)
+
+                        i += 1
+                        print("end with input: '" + file + "' -----------------------\n")
+
+                    end_time = time.time()
+                    logs.write(f"\ntime: {end_time - start_time} sec\n")
+                    print(f"generated logs are written in logstt/logs_{s}.txt")
+                    print(f"time: {end_time - start_time} sec")
+
+                    prog = open(f"logstt/progress.txt", 'a')
+                    prog.write(f"\nfinished.\n")
+                    prog.close()
+
+
+run_all()
