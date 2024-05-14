a4d36325640c6e214677815755fd60ee9c6da9ee
Johannes von Geisau <johannes.von.geisau@t-online.de>
2024-05-14 15:38:09 +0200

    eval stuff againnnnn

--- src/start_big_eval2.py
+++ src/start_big_eval2.py
@@ -22,9 +22,9 @@ def split_list_into_sublists(lst, x):
 
 files = os.listdir("./benchmarks/all_benchmarks_with_opt")
 for config in ["two-job-random-swap,improvement,max",
-               "two-job-best-swap,improvement,8 two-job-best-swap,improvement-or-rs-by-5%-chance,8"
-               "two-job-best-swap,improvement,4 two-job-best-swap,improvement-or-rs-by-5%-chance,4 two-job-random-swap,improvement,4 two-job-random-swap,decline-by-5%-chance,4"
-               "two-job-best-swap,improvement-or-rs-by-5%-chance,8 two-job-random-swap,improvement,8"
+               "two-job-best-swap,improvement,8 two-job-best-swap,improvement-or-rs-by-5%-chance,8",
+               "two-job-best-swap,improvement,4 two-job-best-swap,improvement-or-rs-by-5%-chance,4 two-job-random-swap,improvement,4 two-job-random-swap,decline-by-5%-chance,4",
+               "two-job-best-swap,improvement-or-rs-by-5%-chance,8 two-job-random-swap,improvement,8",
                "two-job-best-swap,improvement,4 two-job-best-swap,improvement-or-rs-by-5%-chance,8 two-job-random-swap,improvement,2 two-job-random-swap,decline-by-5%-chance,2"]:
     for threads in [16]:
         if (
@@ -34,11 +34,11 @@ for config in ["two-job-random-swap,improvement,max",
             for i in range(files_subsets.__len__()):
                 print("leggo", threads, i)
                 # run_all(files_subsets[i], i,threads)
-                with open(f"tmp2_{threads}_{i}.txt", 'w') as f:
+                with open(f"tmp3_{threads}_{i}.txt", 'w') as f:
                     for file in files_subsets[i]:
                         f.write(file + '\n')
                 p = subprocess.Popen(["python3", "-c",
-                                      f"from src.run_all_parameterized_long2 import run_all; run_all({[f'tmp2_{threads}_{i}.txt']}, {i},{threads},\"{config}\")"])
+                                      f"from src.run_all_parameterized_long2 import run_all; run_all({[f'tmp3_{threads}_{i}.txt']}, {i},{threads},\"{config}\")"])
                 processes.append(p)
 
             for p in processes:
